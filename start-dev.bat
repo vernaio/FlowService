9de7046bfa2fd6e4f4716bc543f794592fe19e02
Robert.Meissner <Robert.Meissner@PerfectPattern.de>
2019-11-22 16:33:32 +0100

    SPO console URL is no longer hard coded

--- start-dev.bat
+++ start-dev.bat
@@ -17,11 +17,11 @@ docker run ^
     -v %cd%/data/in:/data/in ^
     -v %cd%/data/out:/data/out ^
     -v %cd%/data/storage:/data/storage ^
-    -e SPO_URL=spo-v3.web-apps.wmdTest.tryout.zone ^
-    -e SPO_TENANT=pib ^
-    -e SPO_USER=vorstufe ^
+    -e SPO_URL=spo-v3.web-apps.opti-beta.cloud ^
+    -e SPO_TENANT=spodemo ^
+    -e SPO_USER=admin ^
     -e SPO_PASSWORD=%password% ^
-    -e SPO_WORKSPACE_ID=test ^
+    -e SPO_WORKSPACE_ID=pib-demo-1 ^
     -e INPUT_LOGIC_NAME=default-pdf-integration ^
     -e INPUT_LOGIC_URL=https://github.com/perfectpattern/DefaultPdfIntegration/archive/1.0.0.tar.gz ^
     -e IMPOSITION_URL=http://192.168.0.224:4200 ^
