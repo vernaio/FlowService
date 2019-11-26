afa23ff3fc98780302a9b536af5de90096be5f0f
Robert.Meissner <Robert.Meissner@PerfectPattern.de>
2019-11-26 12:05:04 +0100

    .

--- start-dev.bat
+++ start-dev.bat
@@ -17,13 +17,13 @@ docker run ^
     -v %cd%/data/in:/data/in ^
     -v %cd%/data/out:/data/out ^
     -v %cd%/data/storage:/data/storage ^
-	-v C:\dev\pib-flows\rindt-integration:/opt/custom-integration ^
+	-v C:\dev\pib-flows\my-integration:/opt/custom-integration ^
     -e SPO_URL=spo-v3.web-apps.opti-beta.cloud ^
     -e SPO_TENANT=spodemo ^
     -e SPO_USER=admin ^
     -e SPO_PASSWORD=%password% ^
     -e SPO_WORKSPACE_ID=pib-demo-1 ^
-    -e INTEGRATION_NAME=rindt-integration ^
+    -e INTEGRATION_NAME=my-integration ^
     -e INTEGRATION_LOCATION=/opt/custom-integration ^
     -e IMPOSITION_URL=http://192.168.0.224:4200 ^
     -it ^
