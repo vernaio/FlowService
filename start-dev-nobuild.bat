ba175b8dd540eef0dc9e880c0cd9615ede85fa78
alxppgithub <alxppgithub@protonmail.ch>
2019-12-19 11:19:18 +0100

    revert builds and trigger build-1

--- start-dev-nobuild.bat
+++ start-dev-nobuild.bat
@@ -1,5 +1,6 @@
 @echo off
-SET version=latest
+SET version=DEV
+SET /P password=Password: 
 
 mkdir data/in
 mkdir data/storage
@@ -11,11 +12,15 @@ docker run ^
     -v %cd%/data/in:/data/in ^
     -v %cd%/data/out:/data/out ^
     -v %cd%/data/storage:/data/storage ^
-    -e SPO_TENANT=testpib ^
-    -e SPO_USER=box ^
-    -e SPO_PASSWORD=NbB08Cv67EFa1p9m4JY2irWJAr3R6v ^
-    -e SPO_WORKSPACE_ID=box ^
-    -e IMPOSITION_URL=http://127.0.0.1:4200 ^
+    -e SPO_URL=spo-v3.web-apps.wmdTest.tryout.zone ^
+    -e SPO_TENANT=pib ^
+    -e SPO_USER=vorstufe ^
+    -e SPO_PASSWORD=%password% ^
+    -e SPO_WORKSPACE_ID=test ^
+    -e INPUT_LOGIC_NAME=default-pdf-integration ^
+    -e INPUT_LOGIC_URL=https://github.com/perfectpattern/DefaultPdfIntegration/archive/1.0.0.tar.gz ^
+    -e IMPOSITION_URL=http://192.168.0.224:4200 ^
     -it ^
     --entrypoint sh ^
-    perfectpattern/flow-service
+    pib-flow:%version%
+
